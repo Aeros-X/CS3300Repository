bb5ad9e951de3631419fb4192e164b6cc5cbee34
Cool user name <npeckham@uccs.edu>
2023-10-12 10:06:54 -0600

    Did stuff successful things IDK

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -118,10 +119,16 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+# Static files (CSS, JavaScript, Images) 
+# https://docs.djangoproject.com/en/4.2/howto/static-files/ 
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' 
+
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'static') 
+] 
+
+MEDIA_URL = '/images/' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
