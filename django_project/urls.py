cf0e55a36f45cee30069a3390c3e2595fd76cf7f
Cool user name <npeckham@uccs.edu>
2023-10-18 12:33:49 -0600

    Adding recent changes

--- django_project/urls.py
+++ django_project/urls.py
@@ -21,4 +21,6 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio_app.urls')),
+    path('students/', views.StudentListView.as_view(), name= 'students'), 
+    path('student/<int:pk>', views.StudentDetailView.as_view(), name='student-detail'),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render 
 from django.http import HttpResponse 
+from django.views import generic
+from .models import Student
 
 # Create your views here. 
 def index(request): 
+    class StudentListView(generic.ListView): 
+        model = Student 
+    class StudentDetailView(generic.DetailView): 
+        model = Student
 
 # Render index.html 
     return render( request, 'portfolio_app/index.html') 
+
+class StudentListView(generic.ListView): 
+    model = Student 
+class StudentDetailView(generic.DetailView): 
+    model = Student
